84151d972aa277e2ecfeea7f9a94ca0ed62ca0e2
Abhishek Sahoo <abhisheksahoo553@gmail.com>
2024-09-02 18:39:20 +0530

    Python

---
+++ Python/28_test.py
@@ -0,0 +1,9 @@
+from customePackage.maths import *            # package
+
+from customePackage.subpackages.mult import multiplication            #sub package
+
+print(addition(12,10))
+print(subtraction(12,10))
+
+print(multiplication(10,21))
+
